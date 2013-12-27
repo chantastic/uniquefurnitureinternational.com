1a028190f50278b2be0e4bb03654db5ec76592a8
Michael Chan <mijoch@gmail.com>
2013-12-27 08:44:29 -0800

    copied from (mt)

---
+++ contact/index.php
@@ -0,0 +1,16 @@
+<?php  # home
+
+//include ('./_inc/url.inc');
+$title = 'about';
+include ('../_inc/head.inc');
+?>
+
+<!-- content:primary -->
+<div id="content">
+	<h1 style="font-family:arial">Contact</h1>
+	<img style="float:right; height: 30%; width=:30%;" src="/_images/contact_img.jpg" alt="">
+	<p>phone - 760.757.2020</p>
+	<p>email - info@uniquefurnitureinternational.com</p>
+</div>
+
+<?php include ('../_inc/foot.inc'); ?>
